fcd47ad9bc215c91e812592a5e9fe46f13a26379
mflorack <140392108+mflorack@users.noreply.github.com>
2023-07-31 08:27:42 +0000

    test

--- .devcontainer/main.java
+++ .devcontainer/main.java
@@ -1,1 +1,7 @@
+// this is a simple program to write hello world to the console
+// to run this program, use the command: javac main.java && java main
+class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
+}
